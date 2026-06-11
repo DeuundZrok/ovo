758b4f7788ce4de0a6bd7bd2a8b3a4a66615368e
Zorik Thorne. <326513871@qq.com>
2026-06-11 16:37:42 +0800

    img
    Image reading, display, and saving

---
+++ img.py
@@ -0,0 +1,23 @@
+import cv2
+from openfile import select_file
+import time
+
+def openimg():
+    #open a iamge
+    img_path=select_file()
+    img=cv2.imread(img_path)
+    cv2.imshow("image",img)
+    key=cv2.waitKey(0)
+    return img,key
+
+def saveimg(img):
+    filename=time.strftime("img_%Y%m%d_%H%M%S.jpg")
+    cv2.imwrite(filename,img)
+    print(f"已保存：{filename}")
+
+img,key=openimg()
+
+if key == 13:
+    saveimg(img)
+
+cv2.destroyAllWindows()                   
